7b0347f87789996b01f7f2ec37a8b42244d9d050
Naveen <enaveen98@gmail.com>
2020-04-24 23:58:13 +0530

    Updates

---
+++ precache-manifest.64f7ec7ff9e8f41cd0983ca772b1a6da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72b2826c962655301dd01592947d75a",
+    "url": "/drummachine/index.html"
+  },
+  {
+    "revision": "2c52ce20e48b14e14f56",
+    "url": "/drummachine/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "917a7a5d459277861282",
+    "url": "/drummachine/static/js/2.a6d12791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drummachine/static/js/2.a6d12791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c52ce20e48b14e14f56",
+    "url": "/drummachine/static/js/main.aed08359.chunk.js"
+  },
+  {
+    "revision": "56575092dcdaff8b5c80",
+    "url": "/drummachine/static/js/runtime-main.98b0f2af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drummachine/static/media/logo.5d5d9eef.svg"
+  }
+]);
